d5ac078d602f95af1aa273b9066b7ab8ceba4b08
Rafli Fattah Nur Ikhsan <133248578+RafliF1@users.noreply.github.com>
2024-05-19 21:44:44 +0700

    Create update_product.php

---
+++ update_product.php
@@ -0,0 +1,18 @@
+<?php
+include 'db_connect.php';
+
+$id = $_POST['id'];
+$name = $_POST['name'];
+$type = $_POST['type'];
+$price = $_POST['price'];
+
+$sql = "UPDATE products SET name='$name', type='$type', price='$price' WHERE id=$id";
+if (mysqli_query($conn, $sql)) {
+    echo "Produk berhasil diupdate.";
+    header("Location:index.php");
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
