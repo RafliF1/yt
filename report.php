06c42dfd48c01b88478d464656f4922b51ce897a
Rafli Fattah Nur Ikhsan <133248578+RafliF1@users.noreply.github.com>
2024-05-20 10:51:17 +0700

    Update report.php

--- report.php
+++ report.php
@@ -156,8 +156,8 @@
         datasets: [{
             label: 'Penjualan Bulanan',
             data: <?php echo json_encode($monthlyData); ?>,
-            backgroundColor: 'rgba(255, 206, 86, 0.2)',
-            borderColor: 'rgba(255, 206, 86, 1)',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            borderColor: 'rgba(255, 0, 0, 1)',
             borderWidth: 1
         }]
     };
