ac3575c57d734aa5b0521111a3c3095f03320a6c
Rafli Fattah Nur Ikhsan <133248578+RafliF1@users.noreply.github.com>
2024-05-28 08:24:47 +0700

    Update edit_product.php

--- product/edit_product.php
+++ product/edit_product.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>Edit Produk</title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
 </head>
 
 <body>
